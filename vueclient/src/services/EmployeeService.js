31cb18fbe2a2fd935ba0991d40cb85e2ab41cc50
aissatna <aissatna.dev@gmail.com>
2024-09-05 01:19:59 +0200

    feat-front : init vue client

---
+++ vueclient/src/services/EmployeeService.js
@@ -0,0 +1,9 @@
+import axios from 'axios'
+const EMPLOYEE_API_BASE_URL = 'http://localhost:8080/api/employees'
+
+class EmployeeService {
+   getEmployees() {
+    return axios.get(EMPLOYEE_API_BASE_URL)
+  }
+}
+export default new EmployeeService()
